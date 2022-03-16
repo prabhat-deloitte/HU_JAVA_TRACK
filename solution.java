9ba33ac1a25e846257536b934be40e5d209197ce
prabhat-deloitte <praparihar@deloitte.com>
2022-03-16 14:54:57 +0530

    fist mini assignment

---
+++ solution.java
@@ -0,0 +1,23 @@
+class Area {
+    void rect(float length, float breadth){
+        System.out.println("the area of the rectangle is "+length*breadth+" sq units");
+    }
+    void circle(float radius){
+        double area = 3.14 *radius *radius;
+        System.out.println("the area of the circle is "+area+" sq units");
+    }void square(float length){
+        System.out.println("the area of the square is "+Math.pow(length, 2)+" sq units");
+    }
+}
+
+class solution{
+    public static void main(String[] args) {
+        //creating a object of
+        Area ob = new Area();
+        ob.circle(2);
+        ob.rect(2,2 );
+        ob.square(2);
+
+
+    }
+}
