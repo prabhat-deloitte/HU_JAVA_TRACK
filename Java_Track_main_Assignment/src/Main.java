d51fd81905afc00023a4ddd9ebcbeba68f5712e6
prabhat-deloitte <praparihar@deloitte.com>
2022-03-22 07:36:22 +0530

    Created_ registration

--- Java_Track_main_Assignment/src/Main.java
+++ Java_Track_main_Assignment/src/Main.java
@@ -22,7 +22,7 @@ class Csv {
                 String [] list1 = line.split(",");
 
 
-                for (int i =0; i<5; i++){
+                for (int i =0; i<2; i++){
 
                     inner_list.add(list1[i]);
                 }
@@ -45,30 +45,20 @@ public class Main{
 
         System.out.println("1.Login \n 2.registration");
         Scanner scn = new Scanner(System.in);
-        String username,re_enter_pass, password;
-        System.out.println("Enter User name");
-        username = scn.next();
-        String validity = "Invalid";
-        while(validity == "Invalid"){
-        System.out.println("Enter password •\tPassword should be in a range of 5 to 10 characters with below combination  \n" +
-                "\t     At least one Uppercase alphabet  \n" +
-                "\t     At least one Lowercase alphabet  \n" +
-                "\t     At least one special characters “@ #  &  % * ! ”  \n" +
-                "\t     At least one digit 1,2,3,…. \n");
-        password = scn.next();
-        Password pass = new Password();
-        try {
-            if (Password.validatePassword(password).equals("Valid password")){
-                validity = "Valid";
+       int choice = scn.nextInt();
+
+         if (choice == 2) {
+            Registration registration = new Registration();
+            try {
+                Registration.register();
+            }
+            catch(Exception e){
+                System.out.println(e);
             }
         }
-        catch(Password.PasswordException e){
-            System.out.println(e);
-        }
-
-        Login log = new Login();
-        System.out.println(log.check_user("RIYA SINGH"));
+        //Login log = new Login();
+        //System.out.println(log.check_user("RIYA SINGH"));
 
     }
-}}
+}
 
--- Java_Track_main_Assignment/src/Registration.java
+++ Java_Track_main_Assignment/src/Registration.java
@@ -1,32 +1,52 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Scanner;
 
 public class Registration {
-    public static void main(String[] args) {
 
-        try (PrintWriter writer = new PrintWriter("C:\\Users\\praparihar\\IdeaProjects\\Assignment 5\\HU_JAVA_TRACK\\Java_Track_main_Assignment\\src\\Book1.csv")) {
+    static void register () throws IOException{
+        try {
+            String username,re_enter_pass, password;
+            System.out.println("Enter User name");
+            Scanner scn = new Scanner(System.in);
+            username = scn.next();
+            String validity = "Invalid";
+            String pswrd;
+            while(validity == "Invalid"){
+                System.out.println("Enter password •\tPassword should be in a range of 5 to 10 characters with below combination  \n" +
+                        "\t     At least one Uppercase alphabet  \n" +
+                        "\t     At least one Lowercase alphabet  \n" +
+                        "\t     At least one special characters “@ #  &  % * ! ”  \n" +
+                        "\t     At least one digit 1,2,3,…. \n");
+                pswrd = scn.next();
+                Password pass = new Password();
+                try {
+                    if (Password.validatePassword(pswrd).equals("Valid password")){
+                        password = pswrd;
+                        FileWriter fileWriter =new FileWriter("C:\\Users\\praparihar\\IdeaProjects\\Assignment 5\\HU_JAVA_TRACK\\Java_Track_main_Assignment\\src\\Book1.csv", true);
+                        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(username);
+                        sb.append(',');
+                        sb.append(password);
+                        sb.append('\n');
+                        bufferedWriter.write(sb.toString());
+                        bufferedWriter.close();
+                        password = pswrd;
+                        validity = "Valid";
+                    }
+                }
+                catch(Password.PasswordException e){
+                    System.out.println(e);
+                }}
 
-            StringBuilder sb = new StringBuilder();
-            sb.append("id");
-            sb.append(',');
-            sb.append("Name");
-            sb.append('\n');
 
-            sb.append("1");
-            sb.append(',');
-            sb.append("Prashant Ghimireghthsdhseg");
-            sb.append('\n');
-
-            writer.write(sb.toString());
-
-            System.out.println(sb);
-
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-
     }
+
 }
+
 
 
